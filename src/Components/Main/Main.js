519161e4e81572eb60dbecb7c38192fa21d61449
Bob <bob.e.baxter@gmail.com>
2019-10-05 10:27:23 -0500

    wip

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Venn from 'venn.js';
+import * as d3 from 'd3';
 
 class Main extends React.Component {
+  sets = [
+    { sets: ['A'], size: 12 },
+    { sets: ['B'], size: 12 },
+    { sets: ['A', 'B'], size: 2 },
+  ];
+
+  chart = Venn.VennDiagram();
+
   render() {
     return (
         <div>
-            Main
+            {d3.select('#venn').datum(this.sets).call(this.chart)}
         </div>
     );
   }
