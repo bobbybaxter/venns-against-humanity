1e0c9f41bd0c0fab7a07b0c65ca2ac079f575fa4
Bob <bob.e.baxter@gmail.com>
2019-10-08 22:38:06 -0500

    text is now wrapping correctly

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -27,6 +27,44 @@ class Main extends React.Component {
     const { blackCards, whiteCards } = this.state;
     const randomCards = [];
     const circleA = whiteCards[Math.floor(Math.random() * whiteCards.length)];
+    // const label = blackCards[Math.floor(Math.random() * blackCards.length)];
+
+    // const text = '';
+    // const width = 25;
+    // const words = label.split(/\s+/).reverse();
+    // console.error('words', words);
+    // const maxLines = 5;
+    // const minChars = (label.length + words.length) / maxLines;
+    // console.error('minChars', minChars);
+    // let word = words.pop();
+    // console.error('word', word);
+    // let line = [word];
+    // console.error('line', line);
+    // let joined;
+    // console.error('joined', joined);
+    // let lineNumber = 0;
+    // // const lineHeight = 1.1; // ems
+    // let tspan = [];
+    // tspan.push(word);
+    // // console.error('tspan', tspan);
+    // // eslint-disable-next-line no-constant-condition
+    // while (true) {
+    //   word = words.pop();
+    //   if (!word) break;
+    //   line.push(word);
+    //   joined = line.join(' ');
+    //   tspan.push(joined);
+    //   if (joined.length > minChars && tspan.node().getComputedTextLength() > width) {
+    //     line.pop();
+    //     tspan.text(line.join(' '));
+    //     line = [word];
+    //     tspan = text.append('tspan').text(word);
+    //     // eslint-disable-next-line no-plusplus
+    //     lineNumber += 1;
+    //     console.error(lineNumber);
+    //   }
+    // }
+
     const circleB = whiteCards[Math.floor(Math.random() * whiteCards.length)];
     const circleC = whiteCards[Math.floor(Math.random() * whiteCards.length)];
     const circleAB = blackCards[Math.floor(Math.random() * blackCards.length)];
@@ -57,7 +95,7 @@ class Main extends React.Component {
       { sets: [B, C], size: intersectionCircleSize, label: BC },
       { sets: [A, B, C], size: centerCircleSize, label: ABC },
     ];
-    const chart = venn.VennDiagram().width(1200).height(700);
+    const chart = venn.VennDiagram().width(700).height(700);
     d3.select(this.refs.venn)
       .datum(sets)
       .call(chart);
