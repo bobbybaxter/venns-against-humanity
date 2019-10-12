b913a368219ffebe708342c85d1c449f147299ec
Bob <bob.e.baxter@gmail.com>
2019-10-12 09:32:08 -0500

    Updates

---
+++ precache-manifest.1cf6a31a3343a748330059d98b6a46a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3563158c355c43da8da9fc5fe3223c15",
+    "url": "/venns-against-humanity/index.html"
+  },
+  {
+    "revision": "a0519eef8b0ac13d1873",
+    "url": "/venns-against-humanity/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a093ce64318a94e1d486",
+    "url": "/venns-against-humanity/static/css/main.f5c24648.chunk.css"
+  },
+  {
+    "revision": "a0519eef8b0ac13d1873",
+    "url": "/venns-against-humanity/static/js/2.0a9f0527.chunk.js"
+  },
+  {
+    "revision": "a093ce64318a94e1d486",
+    "url": "/venns-against-humanity/static/js/main.3bcf1703.chunk.js"
+  },
+  {
+    "revision": "96168403928410090e48",
+    "url": "/venns-against-humanity/static/js/runtime-main.fae59b7e.js"
+  }
+]);
